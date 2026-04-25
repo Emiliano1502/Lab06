560c0603cd346aef8663c8fabaaba03fd3c6db57
Emiliano Ruiz Garcia <32943533+Emiliano1502@users.noreply.github.com>
2026-04-25 01:14:52 +0000

    Se acabo intereses

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Header from "./Components/Header/Header";
-
+import Hero from "./Components/Hero/Hero";
 import CV from "./Components/CV/CV";
+import InterestsSubjects from "./Components/InterestsSubjects/InterestsSubjects";
+
 
 export default function Home() {
   return (
     <>
       <Header />
-
+      <Hero />
+      <InterestsSubjects />
       <CV />
-
+      
 
       <a href="#home" className="scroll-top" id="scroll-top">
         <i className="fas fa-arrow-up"></i>
